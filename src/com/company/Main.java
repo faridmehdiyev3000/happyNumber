5382796f3990d51a1ccd6b3e5e11372e73bebf89
faridmehdiyev3000 <faridm3001@gmail.com>
2022-10-04 08:01:40 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n, r = 1, num, sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ededi daxil edin");
+        n = sc.nextInt();
+        num = n;
+        while (num > 9)
+        {
+            while (num > 0)
+            {
+                r = num % 10;
+                sum = sum + (r * r);
+                num = num / 10;
+            }
+            num = sum;
+            sum = 0;
+        }
+        if (num == 1)
+        {
+            System.out.println("Happy Number");
+        }
+        else
+        {
+            System.out.println("Not Happy Number");
+
+
+    }
+}
+
